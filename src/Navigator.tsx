390e3d973d6f5a6f36c921758ce4470749c0076a
Alean <alean.asa@hotmail.com>
2021-04-06 01:02:25 +0100

    improvements on navigator

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,85 +1,51 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Feed from './screens/Feed';
 
-const Tab = createBottomTabNavigator();
-
-const MenuNavigator = () => {
-  return (
-    <Tab.Navigator initialRouteName="Feed">
-      <Tab.Screen
-        name="Feed"
-        component={Feed}
-        options={{
-          tabBarLabel: 'Feed',
-          tabBarIcon: ({color}) => <Icon name="home" size={30} color={color} />,
-        }}
-      />
-
-      <Tab.Screen
-        name="AddPhoto"
-        component={Feed}
-        options={{
-          tabBarLabel: 'Add picture',
-          tabBarIcon: ({color}) => (
-            <Icon name="camera" size={30} color={color} />
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Profile"
-        component={Feed}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({color}) => <Icon name="user" size={30} color={color} />,
-        }}
-      />
-    </Tab.Navigator>
-  );
+const MenuRoutes = {
+  Feed: {
+    name: 'Feed',
+    screen: Feed,
+    navigationOptions: {
+      title: 'Feed',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="home" size={30} color={tintColor} />
+      ),
+    },
+  },
+  Add: {
+    name: 'AddPhoto',
+    screen: Feed,
+    navigationOptions: {
+      title: 'Add picture',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="camera" size={30} color={tintColor} />
+      ),
+    },
+  },
+  Profile: {
+    name: 'Profile',
+    screen: Feed,
+    navigationOptions: {
+      title: 'Profile',
+      tabBarIcon: ({tintColor}) => (
+        <Icon name="user" size={30} color={tintColor} />
+      ),
+    },
+  },
 };
 
-// const MenuRoutes = {
-//   Feed: {
-//     name: 'Feed',
-//     screen: Feed,
-//     navigationOptions: {
-//       title: 'Feed',
-//       tabBarIcon: ({tintColor}) => (
-//         <Icon name="home" size={30} color={tintColor} />
-//       ),
-//     },
-//   },
-//   Add: {
-//     name: 'AddPhoto',
-//     screen: Feed,
-//     navigationOptions: {
-//       title: 'Add picture',
-//       tabBarIcon: ({tintColor}) => (
-//         <Icon name="camera" size={30} color={tintColor} />
-//       ),
-//     },
-//   },
-//   Profile: {
-//     name: 'Profile',
-//     screen: Feed,
-//     navigationOptions: {
-//       title: 'Profile',
-//       tabBarIcon: ({tintColor}) => (
-//         <Icon name="user" size={30} color={tintColor} />
-//       ),
-//     },
-//   },
-// };
+const MenuConfig = {
+  initialRouteName: 'Feed',
+  tabBarOptions: {
+    showLabel: false,
+  },
+};
 
-// const MenuConfig = {
-//   initialRouteName: 'Feed',
-//   tabBarOptions: {
-//     showLabel: false,
-//   },
-// };
-
-// const MenuNavigator = createBottomTabNavigator(MenuRoutes, MenuConfig);
+const MenuNavigator = createAppContainer(
+  createBottomTabNavigator(MenuRoutes, MenuConfig),
+);
 
 export default MenuNavigator;
